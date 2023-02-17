d70abe90a5492fccf5c83c861e14c244f90a4293
root <6457@holbertonstudents.com>
2023-02-17 04:50:22 -0800

    Task 5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - A program that print a line using the printf f function
  * Return: 0 (success)
@@ -6,7 +7,7 @@
 
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art, \n");
+  printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
 
