ad275b20616ce00e5c632053cfa7c8625b6eb906
root <6457@holbertonstudents.com>
2023-02-22 01:53:18 -0800

    Task 2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,21 @@
-#include <stdio.h>
+#include<stdio.h>
 #include <stdlib.h>
-#include <time.h>
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	char low;
-
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-		putchar('\n');
-
+	char ch = 'a';
+	int i = 0;
+	/* your code goes there */
+	while (i < 26)
+	{
+		putchar(ch);
+		ch++;
+		i++;
+	}
+	putchar('\n');
 	return (0);
 }
