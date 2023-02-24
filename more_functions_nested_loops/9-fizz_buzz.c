526720269121512eefddef05284c86544fdd3b38
root <6457@holbertonstudents.com>
2023-02-23 23:47:46 -0800

    Task 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,21 +1,48 @@
-#include "main.h"
-/**
- * print_square - draws a square using multiple terminal lines and #'s.
- * @n: the number of # to print in each line.
- */
-void print_square(int n)
-{
-	int i, j;
+#include <stdio.h>
 
-	if (n > 0)
+/**
+ * main - Prints the numbers from 1-100, but for multiples of three,
+ *        Fizz is printed instead of the number, for multiples of five,
+ *        Buzz, and for multiples of both three and five, FizzBuzz.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
 	{
-		for (i = 0; i < n; i++)
-		{
-			for (j = 0; j < n; j++)
-				_putchar('#');
-			_putchar('\n');
-		}
+		if ((num % 3) == 0 && (num % 5) == 0)
+			printf("FizzBuzz");
+
+		else if ((num % 3) == 0)
+			printf("Fizz");
+
+		else if ((num % 5) == 0)
+			printf("Buzz");
+
+		else
+			printf("%d", num);
+
+		if (num == 100)
+			continue;
+		printf(" ");
 	}
-	else
-		_putchar('\n');
+
+	printf("\n");
+
+	return (0);
 }
+
+
+
+
+
+
+
+
+
+
+
+
