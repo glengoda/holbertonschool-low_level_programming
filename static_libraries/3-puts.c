3a7eff13c616df85c1e3f165ac61ff5b9d5e7322
root <6457@holbertonstudents.com>
2023-03-09 04:09:41 -0800

    correct

--- static_libraries/3-puts.c
+++ static_libraries/3-puts.c
@@ -4,7 +4,7 @@
  * main - A program that prints a line with puts function
  * Return: 0 (success)
  */
-int main(void)
+void _put main(void)
 {
 _puts("\"Programming is like building a multilingual puzzle");
 return (0);
