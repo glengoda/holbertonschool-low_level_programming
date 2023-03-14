9a32e3c7c51f9695d710e887719407d808f2cef0
root <6457@holbertonstudents.com>
2023-03-14 06:56:38 -0700

    correct

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,39 +1,29 @@
+#include "holberton.h"
 #include <stdlib.h>
-#include "main.h"
 /**
- * *_memset - fills memory with a constant byte
- * @s: memory area to be filled
- * @b: char to copy
- * @n: number of times to copy b
+ * _calloc - allocates memory for an array.
+ * @nmemb: number of elements.
+ * @size: size of bytes.
  *
- * Return: pointer to the memory area s
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-int i;
-for (i = 0; i < n; i++)
-{
-s[i] = b;
-}
-return (s);
-}
-/**
- * *_calloc - allocates memory for an array
- * @nmemb: number of elements in the array
- * @size: size of each element
- *
- * Return: pointer to allocated memory
+ * Return: pointer to the allocated memory.
+ * if nmemb or size is 0, returns NULL.
+ * if malloc fails, returns NULL.
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-char *ptr;
-if (nmemb == 1 || size == 0)
+	char *p;
+	unsigned int i;
 
-return (NULL);
-ptr = malloc(size * nmemb);
-if (ptr == NULL)
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 
-return (NULL);
-_memset(ptr, 0, nmemb * size);
-return (ptr);
+	p = malloc(nmemb * size);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = 0;
+
+	return (p);
 }
--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -1,33 +1,29 @@
-#include "main.h"
+#include "holberton.h"
 #include <stdlib.h>
-
 /**
- * array_range - creates an array of integers
+ * array_range - creates an array of integers.
+ * @min: minimum value.
+ * @max: maximum value.
  *
- * @min: the smallest integer in the array
- * @max: the largest integer in the array
- *
- * Return: the pointer to the newly created array, NULL if min > max,
- * NULL if malloc fails
+ * Return: pointer to the newly created array.
+ * if man > mix, returns NULL.
+ * if malloc fails, returns NULL.
  */
-
 int *array_range(int min, int max)
 {
 	int *ar;
 	int i;
-	int j = 0;
 
 	if (min > max)
 		return (NULL);
 
-	ar = malloc(sizeof(int) * (max - min + 1));
+	ar = malloc(sizeof(*ar) * ((max - min) + 1));
+
 	if (ar == NULL)
 		return (NULL);
 
-	for (i = min; i <= max; i++)
-	{
-		ar[j] = i;
-		j++;
-	}
+	for (i = 0; min <= max; i++, min++)
+		ar[i] = min;
 
 	return (ar);
+}
